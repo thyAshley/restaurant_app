fb910b108afdbfc810392410f0a2fab1bd5cc694
Ashley <thy.ashley@gmail.com>
2020-11-14 13:33:22 +0800

    added password hash using hashsync and gensaltsync

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,37 +1,45 @@
 import User from '../models/userModel'
 import * as dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
 dotenv.config();
 
 import connectDb from '../db';
 
-connectDb();
 const userData = [
   {
     name: 'Joe',
     email: 'Joe@gmail.com',
-    password: 123
+    password: bcrypt.hashSync('123123', bcrypt.genSaltSync(10))
   },
   {
     name: 'John',
     email: 'John@gmail.com',
-    password: 123
+    password: bcrypt.hashSync('123123', bcrypt.genSaltSync(10))
+  },
+  {
+    name: 'Admin',
+    email: 'admin@test.com',
+    password: bcrypt.hashSync('123123', bcrypt.genSaltSync(10)),
+    isAdmin: true
   }
 ]
 
 const seedUser = async () => {
-  console.log('test')
   try {
-    const createdUser = await User.insertMany(userData);
-    console.log(createdUser);
+    await connectDb();
+    console.log('deleting existing users')
+    await User.deleteMany({})
+    console.log('adding new seeded users')
+    await User.insertMany(userData);
+    console.log('user seeding completed, process exiting')
   } catch (error) {
     console.log(error)
   }
-  console.log('completed seeding')
   process.exit(1);
 }
 
-if (process.argv[2] === '-d') {
-  console.log('fail')
-} else {
+if (process.argv[2] === 'populate') {
   seedUser()
+} else {
+  console.log('populate to insert data')
 }
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -3,6 +3,8 @@ import {IUser} from '../types/models'
 
 interface IUserDocument extends IUser, Document {}
 
+import bcryptjs from 'bcryptjs';
+
 const userModel = new mongoose.Schema({
   name: {
     type: String,
@@ -10,11 +12,13 @@ const userModel = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: 6,
   },
   isOwner: {
     type: Boolean,
@@ -26,4 +30,11 @@ const userModel = new mongoose.Schema({
   }
 })
 
+userModel.pre<IUserDocument>("save", async function(next) {
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt)
+  next();
+})
+
 export default mongoose.model<IUserDocument>('User', userModel)
+
