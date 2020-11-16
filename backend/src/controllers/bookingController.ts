45fe9ccb915b90cd187ee38f1e7ab8a660b249d5
ASHLEY TEO <ashleyt@PSA.COM.SG>
2020-11-16 15:56:22 +0800

    added booking api to make booking, missing date/time

--- backend/src/controllers/bookingController.ts
+++ backend/src/controllers/bookingController.ts
@@ -6,16 +6,23 @@ import Restaurant from '../models/restaurantModel'
 
 export const makeBooking = async (req: Request, res: Response, next: NextFunction) => {
     const {date, numberOfPax, time} = req.body;
-    const formatDate = moment(date).format('DD/MM/yyyy');
     const restaurantId = req.params.restaurantId;
-    const formatTime = moment.utc(time, "HH").format('HH:mm')
     try {
         const restaurant = await Restaurant.findById(restaurantId);
         if (!restaurant) {
             console.log(restaurant);
             return next(new Error('Invalid Restaurant ID, please try again'))
         }
-        console.log(restaurant)
+        
+        const bookingDetail = new Booking({
+            userId: res.locals.user._id,
+            restaurantId: restaurant._id,
+            date: moment(date).format('DD/MM/yyyy'),
+            time: moment.utc(time, "HH").format('HH:mm'),
+            numberOfPax
+        })
+        const result = await bookingDetail.save();
+        return res.status(200).json(result);
     } catch (error) {
         return res.send(error.message);
     }
--- backend/src/models/bookingModel.ts
+++ backend/src/models/bookingModel.ts
@@ -19,6 +19,10 @@ const bookingSchema = new mongoose.Schema({
     type: Date,
     required: true
   },
+  time: {
+    type: Date,
+    required: true
+  },
   numberOfPax: {
     type: Number,
     required: true
