7406b3b7f0dad36974b6f5882276d8106c7bd563
Ashley <thy.ashley@gmail.com>
2020-11-16 20:02:10 +0800

    added frontend

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar />
+      <Text>Test</Text>
+      <Text>Test</Text>
+    </View>
+  );
+}
---
+++ frontend/util/color.js
@@ -0,0 +1,8 @@
+const colorScheme = {
+  primary: "#CE6262",
+  secondary: "214252",
+  textLight: "rgba(0,0,0,0.25)",
+  black: "#000000",
+  white: "#FFFFFF",
+};
+export default colorScheme;
