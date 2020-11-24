fff8d78200053439051562a31c7b0cfd79628e19
Ashley Teo <thy.ashley@gmail.com>
2020-11-24 14:21:30 +0800

    added validation and session check

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -7,19 +7,27 @@ export const authMiddleWare = async (req: Request, res:Response, next: NextFunct
     try {
         const token = req.headers.authorization?.split(' ')[1];
         if (token) {
-            const payload = <any>jwt.verify(token, process.env.JWT_SECRET!);
+            const payload = <any>jwt.verify(token, process.env.JWT_SECRET!)
             const user = await User.findById(payload.id).select('-password')
             if (user) {
                 res.locals.user = user;
                 return next();
             } 
         }
-        res.status(401);
-        return next(new Error('You are not authorize or something went wrong, please log in again'))
+        res.status(500);
+        return next(new Error('Something went wrong, please try again'))
         
     } catch (error) {
-        console.error(error)
-       res.status(500);
-       next(new Error('Something went wrong, please try again'))
+        if (error.name === 'TokenExpiredError') {
+            res.status(401);
+            return next(new Error('You session has expired, please log in again'))
+        }
+        res.status(500);
+        next(new Error('Something went wrong, please try again'))
     }
+}
+
+export const session = (req: Request, res:Response, next: NextFunction) => {
+    if (!res.locals.user) next(new Error('You session has expired, please log in again'))
+    res.json(res.locals.user);
 }
--- backend/src/routes/loginRoute.ts
+++ backend/src/routes/loginRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import {body} from 'express-validator';
 import {login, register} from '../controllers/loginController'
-
+import {authMiddleWare, session} from '../middleware/authMiddleware'
 const router = express.Router();
 
+router.post('/auth', authMiddleWare, session);
 router.post('/login', login)
 router.post('/register', 
 [
