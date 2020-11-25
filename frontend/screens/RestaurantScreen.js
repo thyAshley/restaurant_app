9588a174f91b07418f2dfbe98cd293d5f6f7c302
Ashley <thy.ashley@gmail.com>
2020-11-25 21:59:14 +0800

    added booking api

--- frontend/screens/RestaurantScreen.js
+++ frontend/screens/RestaurantScreen.js
@@ -7,16 +7,19 @@ import {
   Image,
   FlatList,
   Dimensions,
-  Button,
   TouchableOpacity,
 } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 
+import { instance } from "../config/axios";
+import authStorage from "../auth/authstorage";
 import UserInputButton from "../components/UserInputButton";
+import colorScheme from "../util/color";
 
 export default function RestaurantScreen({ details, route }) {
+  const { restaurant } = route.params;
   const [date, setDate] = useState(Date.now());
-  const [time, setTime] = useState("");
+  const [time, setTime] = useState(Date.now());
   const [mode, setMode] = useState("date");
   const [show, setShow] = useState(false);
 
@@ -28,6 +31,30 @@ export default function RestaurantScreen({ details, route }) {
     } else {
       const currentTime = selectedDate || time;
       setTime(currentTime);
+    }
+  };
+  const makeBooking = async () => {
+    console.log("making booking");
+    try {
+      const token = await authStorage.getToken();
+      console.log("obtain token");
+      console.log("calling API");
+      const bookings = await instance.post(
+        `/v1/api/Booking/${restaurant._id}`,
+        {
+          date: moment(date).format("yyyy-MM-DD"),
+          time: moment(time).format("HH:mm"),
+          numberOfPax: 2,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(bookings);
+    } catch (error) {
+      console.error(error.message);
     }
   };
 
@@ -49,7 +76,6 @@ export default function RestaurantScreen({ details, route }) {
     showMode("time");
   };
 
-  const { restaurant } = route.params;
   return (
     <View style={styles.container}>
       <View>
@@ -76,15 +102,27 @@ export default function RestaurantScreen({ details, route }) {
       <Text>{restaurant.cuisine}</Text>
       <Text>{restaurant.reviews}</Text>
       <Text>Opening Time:</Text>
-      <Text>Selected Date: {moment(date).format("DD/MM/yyyy")}</Text>
-      <Text>Selected Time: {moment(time).format("hh:mm")}</Text>
-      <TouchableOpacity onPress={showDatepicker}>
-        <Text>Select Date</Text>
+      <View>
+        <View style={styles.booking}>
+          <Text style={styles.bookingText}>
+            Booking Date: {moment(date).format("DD/MM/yyyy")}
+          </Text>
+          <TouchableOpacity onPress={showDatepicker}>
+            <Text style={styles.btn}>Select Date</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.booking}>
+          <Text style={styles.bookingText}>
+            Booking Time: {moment(time).format("hh:mm A")}
+          </Text>
+          <TouchableOpacity onPress={showTimepicker}>
+            <Text style={styles.btn}>Select Time</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity onPress={makeBooking}>
+        <UserInputButton text="Book now" location="center" />
       </TouchableOpacity>
-      <TouchableOpacity onPress={showTimepicker}>
-        <Text>Select Time</Text>
-      </TouchableOpacity>
-      <UserInputButton text="Book now" location="center" />
       {show && (
         <DateTimePicker
           testID="dateTimePicker"
@@ -92,6 +130,7 @@ export default function RestaurantScreen({ details, route }) {
           mode={mode}
           is24Hour={true}
           display="default"
+          minimumDate={Date.now()}
           onChange={onChangeDate}
         />
       )}
@@ -100,6 +139,25 @@ export default function RestaurantScreen({ details, route }) {
 }
 
 const styles = StyleSheet.create({
+  booking: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  bookingText: {
+    color: colorScheme.secondary,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  btn: {
+    backgroundColor: colorScheme.secondary,
+    borderRadius: 50,
+    color: colorScheme.white,
+    margin: 5,
+    padding: 5,
+    textAlign: "center",
+    width: 120,
+  },
   container: {
     flex: 1,
     margin: 10,
